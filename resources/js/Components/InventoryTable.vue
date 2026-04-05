fa1fc0adc36568ab8715011253041bc5b10a9040
charl23432 <cbahandianon@ssct.edu.ph>
2026-04-05 18:48:03 +0800

    tag fix ang quantity ky dli amo

--- resources/js/Components/InventoryTable.vue
+++ resources/js/Components/InventoryTable.vue
@@ -1,78 +1,78 @@
 <template>
-<section class="inventory-table">
+  <section class="inventory-table">
+    <div class="table-header">
+      <span>Item</span>
+      <span>Item Name</span>
+      <span>Available</span>
+      <span>Rented</span>
+      <span>Rental Fee</span>
+      <span>Action</span>
+    </div>
 
-<div class="table-header">
-<span>Item</span>
-<span>Item Name</span>
-<span>Available</span>
-<span>Rented</span>
-<span>Rental Fee</span>
-<span>Action</span>
-</div>
+    <div v-if="items.length">
+      <div
+        v-for="item in items"
+        :key="item.id"
+        class="table-row"
+      >
+        <span>
+          <img
+            v-if="item.image"
+            :src="'/storage/' + item.image"
+          />
+          <span v-else>No Image</span>
+        </span>
 
-<div v-if="items.length">
+        <span class="item-name">
+          {{ item.item_name }}
+        </span>
 
-<div
-v-for="item in items"
-:key="item.id"
-class="table-row"
->
+        <span>
+          {{ item.quantity }}
+          <small v-if="item.quantity == 0" style="color:red;">
+            Out of stock
+          </small>
+        </span>
 
-<span>
-<img
-v-if="item.image"
-:src="'/storage/' + item.image"
-/>
+        <span>
+          {{ item.rented ?? 0 }}
+        </span>
 
-<span v-else>No Image</span>
-</span>
+        <span>
+          ₱{{ item.rental_fee }}
+        </span>
 
-<span class="item-name">
-{{ item.item_name }}
-</span>
+        <span class="action-btns">
+          <router-link
+            :to="{ name: 'EditItemPage', params: { id: item.id } }"
+            class="edit-btn"
+          >
+            Edit
+          </router-link>
 
-<span>
-{{ item.quantity }}
+          <button
+            class="delete-btn"
+            @click="$emit('delete-item', item.id)"
+          >
+            Delete
+          </button>
+        </span>
+      </div>
+    </div>
 
-<small v-if="item.quantity == 0" style="color:red;">
-Out of stock
-</small>
-
-</span>
-
-<span>
-{{ item.rented ?? 0 }}
-</span>
-
-<span>
-₱{{ item.rental_fee }}
-</span>
-
-<span class="action-btns">
-
-<router-link
-  :to="{ name: 'EditItemPage', params: { id: item.id } }" class="edit-btn">
-  Edit
-</router-link>
-
-<button class="delete-btn">Delete</button>
-
-</span>
-
-</div>
-
-</div>
-
-<div v-else style="padding:20px;text-align:center;">
-No items found
-</div>
-
-</section>
+    <div v-else style="padding:20px;text-align:center;">
+      No items found
+    </div>
+  </section>
 </template>
-
 
 <script setup>
 defineProps({
-items: Array
+  items: {
+    type: Array,
+    default: () => []
+  }
 })
+
+defineEmits(['delete-item'])
 </script>
